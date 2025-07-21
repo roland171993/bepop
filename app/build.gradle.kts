81e5742572f491b107d22e85b6de7c651449373f
roland171993 <roland171993@gmail.com>
2025-07-21 14:45:06 +0000

    edited Nav Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.RolandAssoh.stopgalere.ci"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 328
+        versionName = "4.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/stopgalere/navigation/NavGraph.kt
+++ app/src/main/java/com/stopgalere/navigation/NavGraph.kt
@@ -5,45 +5,45 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.stopgalere.R
+//import com.stopgalere.R
 import com.stopgalere.navigation.destinations.Screen
-import com.stopgalere.presentation.ui.about.AboutScreen
-import com.stopgalere.presentation.ui.coverletter.CoverList
-import com.stopgalere.presentation.ui.job.JobList
-import com.stopgalere.presentation.ui.resume.ResumeList
-import com.stopgalere.presentation.ui.main.MainScreen
+//import com.stopgalere.presentation.ui.about.AboutScreen
+//import com.stopgalere.presentation.ui.coverletter.CoverList
+//import com.stopgalere.presentation.ui.job.JobList
+//import com.stopgalere.presentation.ui.resume.ResumeList
+//import com.stopgalere.presentation.ui.main.MainScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController, startDestination = Screen.Main.route) {
-        composable(Screen.Main.route) {
-            MainScreen(
-                navController = navController,
-                title = stringResource(R.string.screen_main_title)
-            )
-        }
-        composable(Screen.JobList.route) {
-            JobList(
-                navController = navController,
-                title = stringResource(R.string.screen_jobs_title)
-            )
-        }
-        composable(Screen.ResumeList.route) {
-            ResumeList(
-                navController = navController,
-                title = stringResource(R.string.screen_resumes_title)
-            )
-        }
-        composable(Screen.CoverList.route) {
-            CoverList(
-                navController = navController,
-                title = stringResource(R.string.screen_coverletters_title)
-            )
-        }
-        composable(Screen.About.route) {
-            AboutScreen(
-                title = stringResource(R.string.screen_about_title)
-            )
-        }
+//        composable(Screen.Main.route) {
+//            MainScreen(
+//                navController = navController,
+//                title = stringResource(R.string.screen_main_title)
+//            )
+//        }
+//        composable(Screen.JobList.route) {
+//            JobList(
+//                navController = navController,
+//                title = stringResource(R.string.screen_jobs_title)
+//            )
+//        }
+//        composable(Screen.ResumeList.route) {
+//            ResumeList(
+//                navController = navController,
+//                title = stringResource(R.string.screen_resumes_title)
+//            )
+//        }
+//        composable(Screen.CoverList.route) {
+//            CoverList(
+//                navController = navController,
+//                title = stringResource(R.string.screen_coverletters_title)
+//            )
+//        }
+//        composable(Screen.About.route) {
+//            AboutScreen(
+//                title = stringResource(R.string.screen_about_title)
+//            )
+//        }
     }
 }
