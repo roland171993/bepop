1b334cb480c7b0521c858222c4bd788a0008431e
roland171993 <roland171993@gmail.com>
2025-08-28 17:29:54 +0000

    Replace M3 icon by FontAwesome Icon

--- app/src/main/java/com/stopgalere/presentation/ui/job/components/JobItem.kt
+++ app/src/main/java/com/stopgalere/presentation/ui/job/components/JobItem.kt
@@ -1,4 +1,4 @@
-package com.stopgalere.presentation.ui.job
+package com.stopgalere.presentation.ui.job.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,14 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.font.FontStyle
@@ -29,6 +26,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.stopgalere.presentation.ui.job.JobUi
 
 @Composable
 fun JobItem(
