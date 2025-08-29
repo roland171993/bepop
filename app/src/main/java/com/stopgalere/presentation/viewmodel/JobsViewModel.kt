8bf6ac29a6f20f2e23db51a70705b99fe1e958f3
roland171993 <roland171993@gmail.com>
2025-08-29 13:34:35 +0000

    Fixing JobDetail Previews UI

--- app/src/main/java/com/stopgalere/presentation/viewmodel/JobsViewModel.kt
+++ app/src/main/java/com/stopgalere/presentation/viewmodel/JobsViewModel.kt
@@ -1,13 +1,13 @@
 package com.stopgalere.presentation.viewmodel
 
-package com.stopgalere.presentation.ui.job
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.map
 import com.stopgalere.domain.repository.JobRepoInterface
+import com.stopgalere.presentation.ui.job.JobUi
+import com.stopgalere.presentation.ui.job.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -18,7 +18,7 @@ class JobsViewModel @Inject constructor(
     private val repo: JobRepoInterface
 ) : ViewModel() {
     val jobs: Flow<PagingData<JobUi>> =
-        repo.jobsPaged(query = null, online = true)
+        repo.getJobs(query = null, online = true)
             .map { pd -> pd.map { it.toUi() } }
             .cachedIn(viewModelScope)
 }
